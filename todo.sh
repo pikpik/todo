bfdfeab3ce15a19ad323c8aafadcc09a3a600c4d
pikpik <pikpik@gmail.com>
2011-10-07 14:21:13 +0000

    And so it began.

---
+++ todo.sh
@@ -0,0 +1,53 @@
+todolist="$HOME/.reminders"
+
+todobase () {
+
+	if [ -f $todolist -ne 1 ]
+	then
+		touch $todolist
+	fi	
+
+}
+
+todo () {
+
+	todobase
+
+
+	if [ "$@" ]
+
+	then
+		echo "$@" >> $todolist
+	else
+		echo
+
+		echo "To do  ( `cat $todolist | wc -l | sed 's/ //g'` )"
+
+		echo
+
+		cat $todolist | {
+
+			while read l; do echo "$l"; echo; done;
+
+		}
+	fi
+}
+
+did () {
+
+	todobase
+
+
+	if [ "$@" ]
+
+	then
+		sed "/${1}/d" $todolist > ${todolist}.new
+
+		mv ${todolist}.new $todolist
+	else
+		echo
+		echo "What have you finished?"
+		echo
+	fi
+
+}
